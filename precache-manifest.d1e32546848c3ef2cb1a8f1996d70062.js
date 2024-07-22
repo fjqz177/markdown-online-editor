707ea576f9273d162a4d293d5364374a111e28ae
fjqz177 <fjqz177@users.noreply.github.com>
2024-07-22 16:35:23 +0000

    deploy: c5d0148182bc8867b238ffde468ca548b1adfbb6

--- precache-manifest.d1e32546848c3ef2cb1a8f1996d70062.js
+++ precache-manifest.d1e32546848c3ef2cb1a8f1996d70062.js
@@ -80,12 +80,12 @@ self.__precacheManifest = [
     "url": "/fonts/league-gothic.9900a464.eot"
   },
   {
-    "revision": "cd382dc8a9d6317864b5810a320effc5",
-    "url": "/fonts/league-gothic.cd382dc8.woff"
-  },
-  {
     "revision": "91295fa87df918411b49b7531da5d558",
     "url": "/fonts/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/fonts/league-gothic.cd382dc8.woff"
   },
   {
     "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
@@ -104,11 +104,11 @@ self.__precacheManifest = [
     "url": "/robots.txt"
   },
   {
-    "revision": "bf35c3805e83f0178f9e224d8d0febca",
-    "url": "/service-worker.js"
-  },
-  {
     "revision": "369f6129748a531567ab3535689be69a",
     "url": "/site.webmanifest"
+  },
+  {
+    "revision": "bf35c3805e83f0178f9e224d8d0febca",
+    "url": "/service-worker.js"
   }
 ];

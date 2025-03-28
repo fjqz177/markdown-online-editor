cc2b2e1c98eed5171b9399404ab50b295af93030
fjqz177 <fjqz177@users.noreply.github.com>
2025-03-28 08:37:46 +0000

    deploy: c5d0148182bc8867b238ffde468ca548b1adfbb6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.4bbd624662c87b195782508756d634a2.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.d1ff40883e857d8a47e94611b05eed7d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 const HTMLToCache = '/';
 const version = 'v1.0.0';

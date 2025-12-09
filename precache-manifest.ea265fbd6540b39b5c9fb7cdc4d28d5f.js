44abe239ad09e638dbc5ea8568ef51da176523c8
fjqz177 <fjqz177@users.noreply.github.com>
2025-12-09 02:21:02 +0000

    deploy: c5d0148182bc8867b238ffde468ca548b1adfbb6

--- precache-manifest.ea265fbd6540b39b5c9fb7cdc4d28d5f.js
+++ precache-manifest.ea265fbd6540b39b5c9fb7cdc4d28d5f.js
@@ -80,12 +80,12 @@ self.__precacheManifest = [
     "url": "/fonts/league-gothic.9900a464.eot"
   },
   {
-    "revision": "cd382dc8a9d6317864b5810a320effc5",
-    "url": "/fonts/league-gothic.cd382dc8.woff"
-  },
-  {
     "revision": "91295fa87df918411b49b7531da5d558",
     "url": "/fonts/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/fonts/league-gothic.cd382dc8.woff"
   },
   {
     "revision": "6f0a76321d30f3c8120915e57f7bd77e",

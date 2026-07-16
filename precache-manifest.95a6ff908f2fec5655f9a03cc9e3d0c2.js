3534b1c295e704c1692f927c9d10af39c5a9c636
fjqz177 <fjqz177@users.noreply.github.com>
2026-07-16 10:22:15 +0000

    deploy: c5d0148182bc8867b238ffde468ca548b1adfbb6

--- precache-manifest.95a6ff908f2fec5655f9a03cc9e3d0c2.js
+++ precache-manifest.95a6ff908f2fec5655f9a03cc9e3d0c2.js
@@ -76,16 +76,16 @@ self.__precacheManifest = [
     "url": "/js/element.d712a617.js"
   },
   {
-    "revision": "9900a4643cc63c5d8f969d2196f72572",
-    "url": "/fonts/league-gothic.9900a464.eot"
-  },
-  {
     "revision": "cd382dc8a9d6317864b5810a320effc5",
     "url": "/fonts/league-gothic.cd382dc8.woff"
   },
   {
     "revision": "91295fa87df918411b49b7531da5d558",
     "url": "/fonts/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "9900a4643cc63c5d8f969d2196f72572",
+    "url": "/fonts/league-gothic.9900a464.eot"
   },
   {
     "revision": "6f0a76321d30f3c8120915e57f7bd77e",
